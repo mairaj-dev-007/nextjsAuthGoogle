68799fd844e1d08c08db788017674b2cc70adff0
mairaj-dev-007 <mairaj.imk.dev@gmail.com>
2025-07-24 17:12:50 +0500

    Update page.js
    abcd

--- src/app/[locale]/game/page.js
+++ src/app/[locale]/game/page.js
@@ -1,5 +1,21 @@
-import Game from '../../game/page';
+"use client"
+import dynamic from 'next/dynamic';
+
+const Game = dynamic(() => import('../../game/page'), {
+  ssr: false,
+  loading: () => (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontSize: '18px'
+    }}>
+      Loading game...
+    </div>
+  )
+});
 
 export default function LocalizedGame() {
   return <Game />;
-} 
+}
